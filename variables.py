31ba36dc2e55bccde4804cf96b45e8282b1f2e34
Sofia Cardona <sofiacaardona911@gmail.com>
2026-09-16 19:13:11 -0600

    commit 2: trabajando con variables

--- variables.py
+++ variables.py
@@ -1,4 +1,9 @@
 # Esta sesion sera para aprender el uso de variables 
-print ("HOLA MUNDO")
-print("HOY ES MIERCOLES")
-print("manana es jueves")
+print ("sistema de mensajeria")
+distancia_km = 40.7
+peso_kg = 2.0
+es_urgente = True
+zona = "B"
+
+distancia_km = distancia_km + 16
+print(distancia_km)
